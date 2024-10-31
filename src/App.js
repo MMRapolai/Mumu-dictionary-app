d06fbed903c65c58541ad9e519341f8045f8be9f
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-31 10:40:07 +0200

    Initial push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+                <p>
+          Hello world
+        </p>
+       <a href="#" className="btn btn-primary">Hello</a> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
