fe362b41c630ef90f49e8a030251a99e49aa0f7e
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-11-09 16:39:26 +0200

    Created a component phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
+    console.log(props.results);
+
     if (props.results) {
-    return <div className="Results"><h2>{props.results.word}</h2>
+    return (<div className="Results"><h2>{props.results.word}</h2>
+
+    <Phonetic phonetic={props.results.phonetic} />
+    
     {props.results.meanings.map(function (meaning, index) {
         return (
             <div key={index}>
             <Meaning meaning={meaning} />
             </div>
-        )
+        );
     })}
-    </div>;
+    </div>
+    );
 } else {
     return null;
 }
