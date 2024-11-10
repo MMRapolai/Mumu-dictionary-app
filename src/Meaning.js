95dcd5bad9b1836c8dad733e9dea6431718778bb
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-11-10 20:15:34 +0200

    added a section

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,23 +5,21 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
     console.log(props.meaning.definition);
     return (
+        <section>
     <div className="Meaning">
-
     <h4>{props.meaning.partOfSpeech}</h4>
 
     <div>
     <p>
-
     {props.meaning.definition}
     <br />
-<section>
     {props.meaning.example}
     <br />
     <Synonyms synonyms={props.meaning.synonyms} />
     
-</section>
 </p>
     </div>
     </div>
+    </section>
         );
     }
