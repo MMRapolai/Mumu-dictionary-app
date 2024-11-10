4177a9f485ec13c483314966a748c612d4e7a665
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-11-10 23:45:21 +0200

    created a photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,40 +1,68 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+    let [keyword, setKeyword] = useState(props.defaultWord);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-   function handleResponse(response) {
+   function handleDictionaryResponse(response) {
     setResults(response.data);
+    setLoaded(true);
    } 
-    function search(event) {
-        event.preventDefault()
-    // documentation:https://www.shecodes.io/learn/apis/dictionary //
+   function handleImageResponse(response) {
+    setPhotos(response.data.photos);
+   }
+    function searchWords(event) {
 
     let apiKey = "57b13ef26a049644t26b08bo9241280d"
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(imageApiUrl).then(handleImageResponse);
     }
-    function handleKeywordChange(event) {
+    function handleSearch(event) {
         setKeyword(event.target.value);
     }
+    function loading() {
+        searchWords();
+}
+    function handleForm(event) {
+        event.preventDefault();
+        searchWords();
+    }
+    if (loaded) {
+
     return (
         <div className="Dictionary">
-            <section>
+
+            <div className="container">
+            
                 <h1>What word do you want to look up?</h1>
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
-                </form>
-                <div className="hint">
+                <div className="row">
+                    <div className="col-sm-4 searchSection">
+            <form onSubmit={handleForm}>
+                <input type="search" onChange={handleSearch}
+                className="searchInput" />
                     suggested words: sunset, wine, yoga, plant...
+                    </form>
                     </div>
-                    </section>
+                    <div className="col-sm-8">
 
                 <Results results={results} />
+                <Photos photos={photos} />
                 </div>
-            
+                </div>
+                </div>
+                </div>
     );
+    } else {
+        loading();
+        return "Loading...";
+    }
 }
