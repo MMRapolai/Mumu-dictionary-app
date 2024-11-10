ca76acc127d86247654c69762ef2ff02c052dfca
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-11-10 21:36:59 +0200

    Updated the footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Dictionary defaultKeyword="sunset"/>
       </main>
       <footer className='text-center'>
-        Coded by Matshelane Rapolai 
+        Coded by<a href='https://github.com/MMRapolai' target='_blank' rel='noreferrer'> Matshelane Rapolai</a> and is <a href='https://github.com/MMRapolai/Mumu-dictionary-app' target='_blank' rel='noreferrer'>open-sourced on GitHub</a> and hosted on <a href='https://muriel-dictionary-app.netlify.app/' target='_blank' rel='noreferrer'>Netlify</a>
       </footer>
       </div>
     </div>
